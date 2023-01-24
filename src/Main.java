d5278534a01fe28adac872333ae5d2b811904d3d
Valery Maltsev <lisowsky@yandex.ru>
2023-01-24 22:24:30 +0300

    Task 1 - 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // task 1 - 3
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -20,6 +21,23 @@ public class Main {
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+
+        // task 4 - 6
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
 
 
     }
